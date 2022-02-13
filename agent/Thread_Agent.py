77ec7c975c445ff64887ffaa0be9d61da44075cf
Robin <roleciq@gmail.com>
2022-02-13 15:20:56 -0500

    C'est bon l'agent commence bien a la première case

--- agent/Thread_Agent.py
+++ agent/Thread_Agent.py
@@ -9,6 +9,7 @@ class Thread_Agent(Thread):
         self.agent = agent
         self.time_break = time_break
         self.sizeMentalState = sizeMentalState
+        self.bool = False
 
     #Fonction lancée lorsque le thread est start()
     def run(self):
@@ -16,25 +17,23 @@ class Thread_Agent(Thread):
             # Get lock to synchronize threads
             threadLock = threading.Lock()
             threadLock.acquire()
-
-            #check if the environnment changed <=> self.agent.objectif == self.agent.Search_Objective()
-            if self.agent.captor.Detect_New_Env:
-                self.agent.objectif = self.agent.Search_Objective()
-                self.agent.Action()
-                self.agent.plan_action = self.agent.ChoiceAlgo(self.sizeMentalState)
-                self.agent.UpdateMentalState()
-                self.agent.Deplacement()
-                self.agent.Action()
-            else:
-                self.agent.Deplacement()
-                self.agent.Action()
+            if self.bool:
+                if self.agent.captor.Detect_New_Env:
+                    self.agent.objectif = self.agent.Search_Objective()
+                    self.agent.Action()
+                    self.agent.plan_action = self.agent.ChoiceAlgo(self.sizeMentalState)
+                    self.agent.UpdateMentalState()
+                    self.agent.Deplacement()
+                    self.agent.Action()
+                else:
+                    self.agent.Deplacement()
+                    self.agent.Action()
 
             # Free lock to release next thread
             threadLock.release()
-
+            self.bool = True
             # Temps d'attente entre les threads afin de mettre à jour l'intercade graphique
             sleep(self.time_break)
-
 
 
 
